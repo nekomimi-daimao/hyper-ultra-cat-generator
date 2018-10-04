b6d354c8888b481bac84172af55146d2fe4c822c
nekomimi-daimao <>
2018-10-04 22:43:49 +0900

    import cats

---
+++ app/src/main/java/plan/militarize/stray/cat/hyperultracatgenerator/MainActivity.java
@@ -0,0 +1,13 @@
+package plan.militarize.stray.cat.hyperultracatgenerator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
