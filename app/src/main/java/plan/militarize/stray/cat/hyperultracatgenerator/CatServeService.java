934af038781118fa2b86d0c3be749379a57b7059
nekomimi-daimao <>
2018-10-16 20:40:36 +0900

    cat is modifications

--- app/src/main/java/plan/militarize/stray/cat/hyperultracatgenerator/CatServeService.java
+++ app/src/main/java/plan/militarize/stray/cat/hyperultracatgenerator/CatServeService.java
@@ -19,7 +19,9 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -56,7 +58,7 @@ public class CatServeService extends IntentService {
     }
 
 
-    public static void startActionSaveCats(Context context, int from, int to) {
+    public static void startActionSaveCats(final Context context, final int from, final int to) {
         Intent intent = new Intent(context, CatServeService.class);
         intent.setAction(ACTION_SAVE_CATS);
         intent.putExtra(EXTRA_NUM_FROM, from);
@@ -65,34 +67,45 @@ public class CatServeService extends IntentService {
     }
 
     @Override
-    protected void onHandleIntent(Intent intent) {
+    protected void onHandleIntent(final Intent intent) {
+        manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID_SAVE_CAT, getString(R.string.save_the_cats), NotificationManager.IMPORTANCE_LOW);
+            manager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID_SAVE_CAT);
+        builder.setContentTitle("now generating cats...")
+                .setSmallIcon(R.drawable.ic_notification_small);
+
+        startForeground(NOTIFICATION_ID_SAVE_CAT_PROGRESS, builder.build());
+
         if (intent == null || !ACTION_SAVE_CATS.equals(intent.getAction())
                 || !intent.hasExtra(EXTRA_NUM_FROM) || !intent.hasExtra(EXTRA_NUM_TO)) {
+            stopSelf();
             return;
         }
 
         final int from = intent.getIntExtra(EXTRA_NUM_FROM, 0);
         final int to = intent.getIntExtra(EXTRA_NUM_TO, 999);
-        handleActionSaveCats(from, to);
+        handleActionSaveCats(builder, from, to);
     }
 
-    private void handleActionSaveCats(final int from, final int to) {
+    private void handleActionSaveCats(NotificationCompat.Builder builder, final int from, final int to) {
 
         final int MAX = to - from + 1;
 
         manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID_SAVE_CAT, getString(R.string.save_the_cats), NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID_SAVE_CAT, getString(R.string.save_the_cats), NotificationManager.IMPORTANCE_LOW);
             manager.createNotificationChannel(channel);
         }
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID_SAVE_CAT);
         builder.setContentTitle("now generating cats...")
                 .setContentText(0 + "/" + MAX)
                 .setSmallIcon(R.drawable.ic_notification_small)
                 .setLargeIcon(BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.ic_launcher))
                 .setColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent))
-                .setAutoCancel(false)
                 .setCategory(NotificationCompat.CATEGORY_PROGRESS)
                 .setWhen(System.currentTimeMillis())
                 .setProgress(MAX, 0, false)
@@ -100,7 +113,8 @@ public class CatServeService extends IntentService {
         startForeground(NOTIFICATION_ID_SAVE_CAT_PROGRESS, builder.build());
 
         Set<Integer> set = new HashSet<>(MAX);
-        List<Future<?>> list = new ArrayList<Future<?>>(MAX);
+        List<Future<String>> list = new ArrayList<Future<String>>(MAX);
+        List<String> pathList = new ArrayList<>(MAX);
         executorService = Executors.newSingleThreadExecutor();
 
         while (set.size() < MAX) {
@@ -119,7 +133,7 @@ public class CatServeService extends IntentService {
 
         for (int count = 0; count < list.size(); count++) {
             try {
-                list.get(count).get();
+                pathList.add(list.get(count).get());
                 builder.setProgress(MAX, count, false)
                         .setContentText(count + "/" + MAX);
                 manager.notify(NOTIFICATION_ID_SAVE_CAT_PROGRESS, builder.build());
@@ -130,17 +144,22 @@ public class CatServeService extends IntentService {
             }
         }
 
+        String[] pathArray = pathList.stream().filter(Objects::nonNull).toArray(String[]::new);
+        MediaScannerConnection.scanFile(getApplicationContext(), pathArray, null, null);
+
         manager.cancel(NOTIFICATION_ID_SAVE_CAT_PROGRESS);
         builder.setProgress(0, 0, false)
                 .setContentTitle("complete! you have " + MAX + " cats!")
                 .setContentText("your android was filled with cats...")
                 .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setCategory(NotificationCompat.CATEGORY_EVENT);
 
         manager.notify(NOTIFICATION_ID_SAVE_CAT_FINISH, builder.build());
+        stopSelf();
     }
 
-    private static class CatSaver implements Runnable {
+    private static class CatSaver implements Callable<String> {
 
         private final long seed;
         private final Context context;
@@ -151,32 +170,29 @@ public class CatServeService extends IntentService {
         }
 
         @Override
-        public void run() {
+        public String call() {
             final File dir = new File(
                     Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
                     context.getString(R.string.directory_name));
             if (!dir.exists() && !dir.mkdirs()) {
-                return;
+                return null;
             }
             Cat cat = new Cat(context, seed);
             final File png = new File(dir, cat.getName().replaceAll("[/ #:]+", "_") + ".png");
             Bitmap bitmap = cat.createBitmap(EXPORT_BITMAP_SIZE, EXPORT_BITMAP_SIZE);
             cat = null;
             if (bitmap == null) {
-                return;
+                return null;
             }
             try {
                 OutputStream os = new FileOutputStream(png);
                 bitmap.compress(Bitmap.CompressFormat.PNG, 0, os);
                 bitmap.recycle();
                 bitmap = null;
-                MediaScannerConnection.scanFile(
-                        context,
-                        new String[]{png.toString()},
-                        new String[]{"image/png"},
-                        null);
+                return png.toString();
             } catch (Exception e) {
                 Log.e(TAG, "fail to save cats...");
+                return null;
             }
         }
     }
